321663beffb24388278790e2b9f8c0c2a8ac0326
silenceafter <a59-info@yandex.ru>
2025-10-30 15:07:01 +0300

    30.10.2025

---
+++ building-a-lexical-analyzer/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace building_a_lexical_analyzer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
